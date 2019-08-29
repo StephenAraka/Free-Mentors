99091820186d3bebdd9a8c8421e70da2fdb4b379
StephenAraka <stevenaraka@gmail.com>
2019-08-29 15:26:09 +0300

    chore: add css files to public

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '/sign-in.html'));
+});
+
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/sign-up.html'));
+});
+
 app.listen(PORT, () => {
     debug(`listening on port ${chalk.blue(PORT)}`);
 });
