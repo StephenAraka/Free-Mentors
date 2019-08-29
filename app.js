e501c1e5b741b645b37bcae96927b3ffb3228657
StephenAraka <stevenaraka@gmail.com>
2019-08-29 14:19:33 +0300

    feature: serve up and style homepage

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const path = require('path');
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(PORT, () => {
