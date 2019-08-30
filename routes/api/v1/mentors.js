02005f96b48ccab8b5890fe3d5e1312fec5ec4fc
StephenAraka <stevenaraka@gmail.com>
2019-08-30 18:17:16 +0300

    feature: post a new mentor

--- routes/api/v1/mentors.js
+++ routes/api/v1/mentors.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const uuid = require('uuid');
+const uuid = require('uuid');
 
 const router = express.Router();
 const mentors = require('../../../src/Mentors');
@@ -18,6 +18,30 @@ router.get('/:id', (req, res) => {
         res.status(200).json(mentors.filter((mentor) => mentor.mentorId === parseInt(req.params.id, 10)));
     } else {
         res.status(400).json({ msg: `Not found mentor with the id of ${req.params.id}` });
+    }
+});
+
+// create a user
+router.post('/', (req, res) => {
+    const newMentor = {
+        id: uuid.v4(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: 'secret',
+        address: req.body.address,
+        bio: req.body.bio,
+        occupation: req.body.occupation,
+        expertise: req.body.expertise,
+        role: 'user'
+    };
+
+    if (!newMentor.firstName || !newMentor.lastName || !newMentor.email) {
+        res.status(400).json({ msg: 'Please include names and email' });
+    } else {
+        mentors.push(newMentor);
+        res.json(mentors);
+        // res.redirect('/');
     }
 });
 
