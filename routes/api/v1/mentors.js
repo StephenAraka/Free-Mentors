da3dd7f6ae86663d33783b62a222c328ae2ec370
StephenAraka <stevenaraka@gmail.com>
2019-08-30 19:23:00 +0300

    feature: update and delete mentor

--- routes/api/v1/mentors.js
+++ routes/api/v1/mentors.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
     }
 });
 
-// create a user
+// create a mentor
 router.post('/', (req, res) => {
     const newMentor = {
         id: uuid.v4(),
@@ -33,7 +33,7 @@ router.post('/', (req, res) => {
         bio: req.body.bio,
         occupation: req.body.occupation,
         expertise: req.body.expertise,
-        role: 'user'
+        role: 'mentor'
     };
 
     if (!newMentor.firstName || !newMentor.lastName || !newMentor.email) {
@@ -42,6 +42,41 @@ router.post('/', (req, res) => {
         mentors.push(newMentor);
         res.json(mentors);
         // res.redirect('/');
+    }
+});
+
+// Update a mentor
+router.put('/:id', (req, res) => {
+    const found = mentors.some((mentor) => mentor.mentorId === parseInt(req.params.id, 10));
+
+    if (found) {
+        const updateMentor = req.body;
+        mentors.forEach((mentor) => {
+            if (mentor.mentorId === parseInt(req.params.id, 10)) {
+                mentor.firstName = updateMentor.firstName ? updateMentor.firstName : mentor.firstName;
+                mentor.lastName = updateMentor.lastName ? updateMentor.lastName : mentor.lastName;
+                mentor.email = updateMentor.email ? updateMentor.email : mentor.email;
+                mentor.address = updateMentor.address ? updateMentor.address : mentor.address;
+                mentor.bio = updateMentor.bio ? updateMentor.bio : mentor.bio;
+                mentor.occupation = updateMentor.occupation ? updateMentor.occupation : mentor.occupation;
+                mentor.expertise = updateMentor.expertise ? updateMentor.expertise : mentor.expertise;
+
+                res.json({ msg: 'mentor updated', mentor });
+            }
+        });
+    } else {
+        res.status(400).json({ msg: `Not found mentor with the id of ${req.params.id}` });
+    }
+});
+
+// delete a mentor
+router.delete('/:id', (req, res) => {
+    const found = mentors.some((mentor) => mentor.mentorId === parseInt(req.params.id, 10));
+
+    if (found) {
+        res.json({ msg: 'mentor deleted', mentors: mentors.filter((mentor) => mentor.mentorId !== parseInt(req.params.id, 10)) });
+    } else {
+        res.status(400).json({ msg: `Not found mentor with the id of ${req.params.id}` });
     }
 });
 
