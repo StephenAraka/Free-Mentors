25ce20d7866b59eb81b64cedeb5bf7a5268ad251
StephenAraka <stevenaraka@gmail.com>
2019-08-30 17:06:34 +0300

    feature: get one user by Id

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -9,4 +9,16 @@ router.get('/', (req, res) => {
     res.json(users);
 });
 
+
+// get single user
+router.get('/:id', (req, res) => {
+    const found = users.some((user) => user.userId === parseInt(req.params.id, 10));
+
+    if (found) {
+        res.status(200).json(users.filter((user) => user.userId === parseInt(req.params.id, 10)));
+    } else {
+        res.status(400).json({ msg: `Not found user with the id of ${req.params.id}` });
+    }
+});
+
 module.exports = router;
