153124413e798e79fcabd3b3d3eaa2ba981289d0
StephenAraka <stevenaraka@gmail.com>
2019-09-05 10:33:04 +0300

    ch: add tests for signup

--- controllers/auth/signinController.js
+++ controllers/auth/signinController.js
@@ -1,37 +1,56 @@
 import * as jwt from 'jsonwebtoken';
+import Joi from '@hapi/joi';
 import secretKey from './Key';
 import users from '../../dummyData/Users';
 import admin from '../../dummyData/Admin';
+
+const schema = Joi.object().keys({
+    email: Joi.string().email({ minDomainSegments: 2 }),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/)
+});
 
 export default class SignInController {
     // login a user
     static signIn(req, res) {
         const { email, password } = req.body;
 
-        if (email && password) {
-            const user = users.find(
-                (user) => user.email === email && user.password === password // TODO: hash
-            );
-
-            if (user) {
-                jwt.sign({ user }, secretKey, (err, token) => {
-                    if (err) {
-                        res.json({
-                            status: 403,
-                            message: 'Forbidden'
-                        });
-                    } else {
-                        res.json({
-                            status: 201,
-                            message: 'User is successfully logged in',
-                            data: {
-                                token
-                            }
-                        });
-                    }
+        Joi.validate({ email, password }, schema, (err) => {
+            if (err) {
+                res.status(403).json({
+                    status: 403,
+                    message: 'Error please enter valid email and password'
                 });
             }
+        });
+
+        const user = users.find(
+            (user) => user.email === email && user.password === password // TODO: hash
+        );
+
+        if (user) {
+            jwt.sign({ user }, secretKey, (err, token) => {
+                if (err) {
+                    res.json({
+                        status: 403,
+                        message: 'Forbidden'
+                    });
+                } else {
+                    res.status(201).json({
+                        status: 201,
+                        message: 'User is successfully logged in',
+                        data: {
+                            token
+                        }
+                    });
+                }
+            });
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: 'Email address not found'
+            });
         }
+
 
         if (email === admin.email && password === admin.password) {
             jwt.sign({ admin }, secretKey, (err, token) => {
---
+++ tests/mockData/signIn.js
@@ -0,0 +1,20 @@
+const mockUser = [
+    {
+        email: 'henry@gmail.com',
+        password: 'secret'
+    },
+    {
+        email: '',
+        password: 'secret'
+    },
+    {
+        email: 'henry@gmail.com',
+        password: ''
+    },
+    {
+        email: '',
+        password: ''
+    },
+];
+
+export default mockUser;
