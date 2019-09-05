c9c0a55f51e3624b95f8343a5efc877a5da7e3f8
StephenAraka <stevenaraka@gmail.com>
2019-09-05 09:27:30 +0300

    ch:refactor controller

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,21 +1,40 @@
-import { Router } from 'express';
 import * as jwt from 'jsonwebtoken';
 import secretKey from './Key';
 import users from '../../dummyData/Users';
 import admin from '../../dummyData/Admin';
 
-const router = Router();
-// login a user
-router.post('/', (req, res) => {
-    const { email, password } = req.body;
+export default class SignInController {
+    // login a user
+    static signIn(req, res) {
+        const { email, password } = req.body;
 
-    if (email && password) {
-        const user = users.find(
-            (user) => user.email === email && user.password === password // TODO: hash
-        );
+        if (email && password) {
+            const user = users.find(
+                (user) => user.email === email && user.password === password // TODO: hash
+            );
 
-        if (user) {
-            jwt.sign({ user }, secretKey, (err, token) => {
+            if (user) {
+                jwt.sign({ user }, secretKey, (err, token) => {
+                    if (err) {
+                        res.json({
+                            status: 403,
+                            message: 'Forbidden'
+                        });
+                    } else {
+                        res.json({
+                            status: 201,
+                            message: 'User is successfully logged in',
+                            data: {
+                                token
+                            }
+                        });
+                    }
+                });
+            }
+        }
+
+        if (email === admin.email && password === admin.password) {
+            jwt.sign({ admin }, secretKey, (err, token) => {
                 if (err) {
                     res.json({
                         status: 403,
@@ -24,7 +43,7 @@ router.post('/', (req, res) => {
                 } else {
                     res.json({
                         status: 201,
-                        message: 'User is successfully logged in',
+                        message: 'Admin is successfully logged in',
                         data: {
                             token
                         }
@@ -33,25 +52,4 @@ router.post('/', (req, res) => {
             });
         }
     }
-
-    if (email === admin.email && password === admin.password) {
-        jwt.sign({ admin }, secretKey, (err, token) => {
-            if (err) {
-                res.json({
-                    status: 403,
-                    message: 'Forbidden'
-                });
-            } else {
-                res.json({
-                    status: 201,
-                    message: 'Admin is successfully logged in',
-                    data: {
-                        token
-                    }
-                });
-            }
-        });
-    }
-});
-
-export default router;
+}
